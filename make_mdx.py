0424cdd70501bbce09c3869b00e935c0329b97ec
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2024-07-17 14:06:16 -0700

    Added RSS feed integration and new model updates

--- make_mdx.py
+++ make_mdx.py
@@ -3,6 +3,7 @@ import json
 import time
 import openai
 import datetime
+import feedparser
 from dotenv import find_dotenv, load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
@@ -13,16 +14,46 @@ from langchain.chains import LLMChain, SequentialChain
 load_dotenv(find_dotenv())
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def create_title(prompt):
+def import_rss_feed(url, max_items=1):
+    feed = feedparser.parse(url)
+    entries = feed.entries[:max_items]
+    rss_items = []
+    
+    for entry in entries:
+        item = {
+            "title": entry.title,
+            "link": entry.link,
+            "description": entry.description,
+            "published": entry.published
+        }
+        rss_items.append(item)
+
+    return rss_items
+
+
+def create_title(rss_item):
     title_template = PromptTemplate(
-        input_variables=["topic"], 
-        template="""You are a professional bloggger. Write me a maximum 11 word blog title with optimal SEO in mind about {topic}
+        input_variables=["title", "url"], 
+        template="""You are a blog writer and your job is to turn the following news article into an unique SEO optimized blog post.
+
+Here is the title of the article: {title}
+
+Here's the URL please read it here:
+{url}
+
+Based on the information given above, please generate an SEO Optimized Article Title.
+
+Parameters:
+
+Max 10 words & 1 sentence flow
+
+DO NOT put quotes around the title.
         """
     )
 
-    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
+    llm = OpenAI(model="gpt-4", temperature=0.9)
     title_memory = ConversationBufferMemory(
-        input_key="topic", memory_key="chat_history"
+        input_key="title", memory_key="chat_history"
     )
     title_chain = LLMChain(
         llm=llm,
@@ -31,33 +62,34 @@ def create_title(prompt):
         output_key="title",
         memory=title_memory,
     )
-    title = title_chain.run(prompt)
+    title = title_chain.run(title=rss_item["title"], url=rss_item["link"])
     print(f"Title: {title}\n")
     return title.strip()
 
 def create_description(title):
-    title_template = PromptTemplate(
-        input_variables=["title"], template="You are a professional bloggger. In one to two sentences write a description with optimal SEO in mind about {title} "
+    description_template = PromptTemplate(
+        input_variables=["title"], 
+        template="You are a professional blogger. In one to two sentences write a description with optimal SEO in mind about {title} "
     )
 
-    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
-    title_memory = ConversationBufferMemory(
+    llm = OpenAI(model="gpt-4o", temperature=0.9)
+    description_memory = ConversationBufferMemory(
         input_key="title", memory_key="chat_history"
     )
-    title_chain = LLMChain(
+    description_chain = LLMChain(
         llm=llm,
-        prompt=title_template,
+        prompt=description_template,
         verbose=True,
         output_key="description",
-        memory=title_memory,
+        memory=description_memory,
     )
-    description = title_chain.run(title)
+    description = description_chain.run(title=title)
     print(f"Description: {description}\n")
     return description.strip()
 
 def create_index(title):
     print("Initializing creation of index...")
-    index_prompt = f""" You are a professional bloggger. Write me a blog outline on a blog called '{title}' with 7 sections. Each section has 4 subtopics, output as a 
+    index_prompt = f""" You are a professional blogger. Write me a blog outline on a blog called '{title}' with 7 sections. Each section has 4 subtopics, output as a 
     json code. Please make sure to not say anything else except output the code. Assuming multiple sections, it should look exactly 
     like the following in terms of structure: 
         {{\"Title\": \"\",\"Sections\": [{{\"Section 1\": \"\",\"Subtopics\": [\"\", \"\", \"\", \"\"]}},{{\"Section 2\": 
@@ -81,7 +113,6 @@ def create_index(title):
 
 def create_section(index):
     json_string = index["text"]
-    # Removed the slicing of the string
     data = json.loads(json_string)
     sections = data["Sections"]
     title = data["Title"]
@@ -93,7 +124,7 @@ def create_section(index):
 
         blog_post += f"## {section_name}\n\n"
         for subtopic in subtopics:
-            llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
+            llm = OpenAI(model="gpt-4o", temperature=0.9)
             subtopic_template = PromptTemplate(
                 input_variables=["blog", "section", "subtopic"],
                 template=""" The following is a blog called '{blog}' that has a section named '{section}' about 350 words each, 
@@ -118,23 +149,63 @@ def create_section(index):
             blog_post += f"{subtopic_text}\n"
     return blog_post
 
+def create_body(rss_url, title):
+    body_prompt = f"""
+    Generate an article on this topic: {rss_url}
+
+    Here is the title of the article:
+    {title}
+
+    You're a blog writer and you are turning the article given above into a unique SEO-optimized blog post. The title is already written so please start with the body of the article.
+
+    Formatting: Use H1, H2, and H3 headers. Use HTML: You choose what to bold and bullet point. Make sure if you want to bold anything to put in <b></b> tags and <li></li> tags for bullet points.
+
+    Parameters: This blog post should be approx. 2000 words long.
+    """
+
+    llm = OpenAI(model="gpt-4o", temperature=0.9)
+    body_template = PromptTemplate(
+        input_variables=["rss_url", "title"],
+        template=body_prompt,
+    )
+
+    body_memory = ConversationBufferMemory(
+        input_key="rss_url", memory_key="chat_history"
+    )
+    body_chain = LLMChain(
+        llm=llm,
+        prompt=body_template,
+        verbose=True,
+        output_key="body",
+        memory=body_memory,
+    )
+    body_text = body_chain.run(rss_url=rss_url, title=title)
+    time.sleep(2)
+
+    return body_text
+
+
 if __name__ == "__main__":
 
-    prompt = input("Enter the topic here: ")
-    # for topic in favorites:
-    #     prompt = topic
+    # prompt = input("Enter the topic here: ")
+    # url = input("Please input the URL that you would like to turn into a blog post")
+    url = 'https://rss.app/feeds/uqWWFSTUYneBNbrq.xml'
 
-    if prompt:
-        # Removed the quote function as it's not defined in the code
-        title = create_title(prompt)
+
+    # if prompt:
+    if url:
+        max_items = 1
+
+        rss_items = import_rss_feed(url, max_items)
+        title = create_title(rss_items[0])
         description = create_description(title)
         index = create_index(title)
-        blog_post = create_section(index)
+        # blog_post = create_section(index)
+        blog_post = create_body(url, title)
 
         title = title.replace('"', '')
         file_title = title.replace(' ', '-')
 
-        # Get today's date
         today = datetime.date.today()
         formatted_date = today.strftime('%Y-%m-%d')
 
@@ -152,4 +223,3 @@ description: '{description}',
 
 export default (props) => <ArticleLayout meta={{meta}} {{...props}} />\n\n""")
             f.write(blog_post)
-
